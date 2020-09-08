54bcc3b9511cd1e68ead5031c7a712ad86e4cf05
Mike Smith <gooseisdead@mac.com>
2020-09-08 00:38:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,11 +11,26 @@ end
 
 
 def join_nested_strings(mixed_data_1)
-  mixed_data_1.each do |obj|
-  mixed_data_1.flatten
-  mixed_data_1.delete_if { |obj| !(obj.is_a? String) }
+  def join_nested_strings(src)
+  string_array = []
+  row = 0
+  while row < src.count do
+    index = 0
+    while index < src[row].count do
+      if src[row][index].class == String
+        string_array << src[row][index]
+      end
+        index += 1
+    end
+    row += 1
+  end
+    string_array.join(" ")
 end
-end
+#   mixed_data_1.each do |obj|
+#   mixed_data_1.flatten
+#   mixed_data_1.delete_if { |obj| !(obj.is_a? String) }
+# end
+# end
 
 
   # src will be an Array of Arrays of Strings and Integers
